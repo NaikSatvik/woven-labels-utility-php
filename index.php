7739f82df6b7fb3c705033a1c4bbb998781afb39
NaikSatvik <satvikdnaik@gmail.com>
2022-06-26 16:25:30 +0530

    UI altered in whole application..

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/main.css" type="text/css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"> -->
+    <title>Woven Labels Utility</title>
 </head>
 <body>
-    <div class="header">
-        <a href="#default" class="logo">AREX Industries Ltd.</a>
-        <div class="header-right">
-            <a class="active" href="index.php">Home</a>
-            <a>|</a>
-            <a href="production-calc.php">Production Calculator</a>
-            <a href="mix-design-calc.php">Mix Design Calculator</a>
-            <a href="proportion.php">Proportion</a>
-            <a href="wl-utility.php">WL Utility</a>
-        </div>
+    <div class="sidebar">
+        <a class="active" href="index.php">Home</a>
+        <a href="production-calc.php">Production Calculator</a>
+        <a href="wl-utility.php">WL Utility</a>
     </div>
-
-    <div style="padding-left:20px">
-        <!-- <h1>Responsive Header</h1>
-        <p>Resize the browser window to see the effect.</p>
-        <p>Some content..</p> -->
+      
+    <div class="content">
+        <!-- <h2>Responsive Sidebar Example</h2>
+        <p>This example use media queries to transform the sidebar to a top navigation bar when the screen size is 700px or less.</p>
+        <p>We have also added a media query for screens that are 400px or less, which will vertically stack and center the navigation links.</p>
+        <h3>Resize the browser window to see the effect.</h3> -->
     </div>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script> -->
 </body>
 </html>
--- production-calc.php
+++ production-calc.php
@@ -7,79 +7,86 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 <body>
-    <div class="header">
-        <a href="#default" class="logo">AREX Industries Ltd.</a>
-        <div class="header-right">
-            <a href="index.php">Home</a>
-            <a>|</a>
-            <a class="active" href="production-calc.php">Production Calculator</a>
-            <a href="mix-design-calc.php">Mix Design Calculator</a>
-            <a href="proportion.php">Proportion</a>
-            <a href="wl-utility.php">WL Utility</a>
+    <div class="sidebar">
+        <a href="index.php">Home</a>
+        <a class="active" href="production-calc.php">Production Calculator</a>
+        <a href="wl-utility.php">WL Utility</a>
+    </div>
+      
+    <div class="content">
+        <div class="row justify-content-start">
+            <div style="border: 2px solid black;" class="col-4">
+                <br>
+                <h2>Input Elements</h2><br>
+                <form action="production-calc.php" method="post">
+                    <div class="mb-3">
+                        <label class="form-label">Label Length</label>
+                        <input type="text" class="form-control" name="LabelLength">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Total Tapes</label>
+                        <input type="text" class="form-control" name="TotalTapes">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Total Quantity</label>
+                        <input type="text" class="form-control" name="TotalQuantity">
+                    </div>
+                    <button type="submit" name="submit" class="btn btn-primary">Calculate</button>
+                </form>
+                <br> 
+            </div>
+            &nbsp;&nbsp;&nbsp;
+            <div style="border: 2px solid green;" class="col-4">
+                <?php
+                    if (isset($_POST['submit'])) {
+                        $labelLength = $_POST["LabelLength"];
+                        $totalTapes = $_POST["TotalTapes"];
+                        $totalQuantity = $_POST["TotalQuantity"];
+
+                        $tq_in_one_meter = $totalTapes * (1000 / $labelLength);
+                        $PreSelect = 1000 / $labelLength;
+                        $QuantitySingleTape = 1000 / $labelLength;
+                        $TotalQuantityMeter = $tq_in_one_meter;
+                        $TotalPreSelect = $totalQuantity / $totalTapes;
+                ?>
+                        <h2>Results</h2><br>
+                        <table>
+                            <tr>
+                                <td>Label Length: <span class="badge bg-info text-dark"><?php echo $labelLength ?></span></td>
+                            </tr>
+                            <tr>
+                                <td>Total Tapes: <span class="badge bg-info text-dark"><?php echo $totalTapes ?></span></td>
+                            </tr>
+                            <tr>
+                                <td>Total Quantity: <span class="badge bg-info text-dark"><?php echo $totalQuantity ?></span></td>
+                            </tr>
+                            <tr><td></td></tr>
+                            <tr><td></td></tr>
+                            <tr><td></td></tr>
+                            <tr>
+                                <td>Pre-Select for 1 meter: <span class="badge bg-success"><?php echo $PreSelect ?></span></td>
+                            </tr>
+                            <tr>
+                                <td>Quantity in Single Tape: <span class="badge bg-success"><?php echo $QuantitySingleTape ?></span></td>
+                            </tr>
+                            <tr>
+                                <td>Total Quantity in 1 meter: <span class="badge bg-success"><?php echo $TotalQuantityMeter ?></span></td>
+                            </tr>
+                            <tr>
+                                <td>Total Pre - Select: <span class="badge bg-success"><?php echo $TotalPreSelect ?></span></td>
+                            </tr>
+                        </table>
+                <?php
+                        
+                    } else {
+                ?>
+                        <span class="badge bg-danger"><?php echo "Enter inputs to calculate results !!"; ?></span>
+                <?php
+                    }
+                ?> 
+            </div>
         </div>
     </div>
-    <br>
-    <div class="main-body">
-        <h2>Input Elements</h2><br>
-        <form action="production-calc.php" method="post">
-            <div class="mb-3">
-                <label class="form-label">Label Length</label>
-                <input type="text" class="form-control" name="LabelLength">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Total Tapes</label>
-                <input type="text" class="form-control" name="TotalTapes">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Total Quantity</label>
-                <input type="text" class="form-control" name="TotalQuantity">
-            </div>
-            <button type="submit" name="submit" class="btn btn-primary">Calculate</button>
-        </form>
-    </div>
-
-    <div class="results">
-        <?php
-            if (isset($_POST['submit'])) {
-                $labelLength = $_POST["LabelLength"];
-                $totalTapes = $_POST["TotalTapes"];
-                $totalQuantity = $_POST["TotalQuantity"];
-
-                $tq_in_one_meter = $totalTapes * (1000 / $labelLength);
-                $PreSelect = 1000 / $labelLength;
-                $QuantitySingleTape = 1000 / $labelLength;
-                $TotalQuantityMeter = $tq_in_one_meter;
-                $TotalPreSelect = $totalQuantity / $totalTapes;
-        ?>
-                <h2>Results</h2><br>
-                <table>
-                    <tr>
-                        <td>Label Length: <span class="badge bg-info text-dark"><?php echo $labelLength ?></span></td>
-                        <td>Pre-Select for 1 meter: <span class="badge bg-success"><?php echo $PreSelect ?></span></td>
-                    </tr>
-                    <tr>
-                        <td>Total Tapes: <span class="badge bg-info text-dark"><?php echo $totalTapes ?></span></td>
-                        <td>Quantity in Single Tape: <span class="badge bg-success"><?php echo $QuantitySingleTape ?></span></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>Total Quantity in 1 meter: <span class="badge bg-success"><?php echo $TotalQuantityMeter ?></span></td>
-                    </tr>
-                    <tr>
-                        <td>Total Quantity: <span class="badge bg-info text-dark"><?php echo $totalQuantity ?></span></td>
-                        <td>Total Pre - Select: <span class="badge bg-success"><?php echo $TotalPreSelect ?></span></td>
-                    </tr>
-                </table>
-        <?php
-                
-            } else {
-        ?>
-                <span class="badge bg-danger"><?php echo "Enter inputs to calculate results !!"; ?></span>
-        <?php
-            }
-        ?>
-    </div>
-
     <!-- Script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
